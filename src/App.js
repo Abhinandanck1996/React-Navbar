b0acbd244cb9ee2bde137bd1e51b9b420e8a3542
Abhinandan CK <abhinandanck1996@gmail.com>
2022-04-21 16:27:22 +0530

    React Navbar

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+import "./components/global.css";
+import SpotifyLogin from './Spotify Component/SpotifyLogin';
+import EmployeeForm from './Employee/EmployeeForm';
+import Promismethod from './Employee/Promismethod';
+import Pagination from './Employee/Pagination';
+import Navbar from './components/Navbar';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
+
+
